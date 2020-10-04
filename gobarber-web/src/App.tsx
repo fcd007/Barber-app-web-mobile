b18bcfa9ea700d1bf5d4d2e464f5aa5cd75fe782
Claudeilton Dantas <fcd007@hotmail.com>
2020-10-04 19:50:53 -0300

    update erros styles signIn

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+// eslint-disable-next-line import/extensions
 import SignIn from './pages/signIn';
 
 const App: React.FC = () => (
--- gobarber-web/src/pages/signIn/index.tsx
+++ gobarber-web/src/pages/signIn/index.tsx
@@ -4,7 +4,6 @@ import { Container, Content, Background } from './styles';
 const SignIn: React.FC = () => (
   <Container>
     <Content></Content>
-
     <Background />
   </Container>
 );
