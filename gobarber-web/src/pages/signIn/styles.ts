7750beae2114a5b4271b898b28e43c2ed657ea48
Claudeilton Dantas <fcd007@hotmail.com>
2020-10-04 22:10:27 -0300

    page signIn complete - styles add

--- gobarber-web/src/pages/signIn/styles.ts
+++ gobarber-web/src/pages/signIn/styles.ts
@@ -1,7 +1,109 @@
 import styled from 'styled-components';
+import { shade }  from 'polished';
 
-export const Container = styled.div``;
+import signBackgroundImg from '../../assets/sign-in-background.png';
+export const Container = styled.div`
+    height: 100vh;
 
-export const Content = styled.div``;
+    display: flex;
+    align-items: stretch;
+`;
 
-export const Background = styled.div``;
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    place-content: center;
+
+    width: 100%;
+    max-width: 700px;
+
+    form {
+        margin: 80px 0;
+        width: 340px;
+
+        text-align: center;
+
+        h1 {
+
+            margin-bottom: 24px;
+        }
+
+        input {
+            font-size: 16px;
+            color: #f4ede8;
+            background: #232129;
+            border-radius: 10px;
+            border: 2px solid #232129;
+            padding: 16px;
+            width: 100%;
+
+            & + input {
+                margin-top: 8px;
+            }
+        }
+
+        button {
+            background: #ff9000;
+            color: #312e38;
+            
+            font-weight: 500;
+
+            border-radius: 10px;
+            border: 0;
+            
+            margin-top: 16px;
+            padding: 0 16px;
+            height: 56px;
+
+            width: 100%;
+
+            transition: background-color 0.2s;
+
+            &:hover {
+                background: ${ shade(0.2, '#ff9000') };
+            }
+        }
+
+        a {
+            color: #f4ede8;
+
+            display: block;
+            margin-top: 24px;
+            text-decoration: none;
+
+            transition: color 0.2s;
+
+            &:hover {
+                color: ${ shade(0.2, '#f4ede8')};
+            }
+        }
+    }
+
+    > a {
+            color: #ff9000;
+
+            margin-top: 24px;
+            display: block;
+            display: flex;
+            text-decoration: none;
+            align-items: center;
+
+            svg {
+                margin-:right: 16px;
+            }
+
+            transition: color 0.2s;
+
+            &:hover {
+                color: ${ shade(0.2, '#ff9000')};
+            }
+        }
+`;
+
+export const Background = styled.div`
+    flex: 1;
+    background: url(${ signBackgroundImg }) no-repeat center;
+    background-size: cover;
+`;
