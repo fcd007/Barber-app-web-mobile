df9087a789ceaf704fcf5f96ca28c66feefdb2c7
Claudeilton Dantas <fcd007@hotmail.com>
2020-10-05 21:51:41 -0300

    data input states form user

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -1,36 +1,44 @@
 import React from 'react';
 import { Container, Content, Background } from './styles';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.svg';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Background />
-    <Content>
-      <img src={ logoImg } alt="GoBarber Web"/>
-      <form >
-        <h1>Faça seu cadastro</h1>
-        
-        <Input name="name" icon={ FiUser } placeholder= "Nome" />
-        <Input name="email" icon={ FiMail } placeholder= "E-mail" />
+const SignUp: React.FC = () => {
 
-        
-        <Input name="password" icon={ FiLock } type="password" placeholder="Senha" />
-        
-        <Button type="submit">Cadastrar</Button>
+  function handleSubmit(data:object): void {
+    console.log(data);
+  } 
 
-      </form>
+  return (
+      <Container>
+      <Background />
+      <Content>
+        <img src={ logoImg } alt="GoBarber Web"/>
+        <Form onSubmit={ handleSubmit }>
+          <h1>Faça seu cadastro</h1>
+          
+          <Input name="name" icon={ FiUser } placeholder= "Nome" />
+          <Input name="email" icon={ FiMail } placeholder= "E-mail" />
 
-      <a href="#">
-        <FiArrowLeft />
-        Voltar para logon
-      </a>
-    </Content>
-  </Container>
-);
+          
+          <Input name="password" icon={ FiLock } type="password" placeholder="Senha" />
+          
+          <Button type="submit">Cadastrar</Button>
+
+        </Form>onSubmit={ }
+
+        <a href="#">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  );
+}
 
 export default SignUp;
