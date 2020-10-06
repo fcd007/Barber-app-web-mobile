fee4ed4f135b871a3a8b3a9b673f838bbc4f4df0
Claudeilton Dantas <fcd007@hotmail.com>
2020-10-06 06:05:36 -0300

    update states input effect useCallback and forms

--- gobarber-web/src/components/Input/index.tsx
+++ gobarber-web/src/components/Input/index.tsx
@@ -1,4 +1,13 @@
-import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
+import React, 
+{ 
+    InputHTMLAttributes, 
+    useEffect, 
+    useRef, 
+    useState,
+    useCallback 
+} from 'react';
+
+
 import { Container } from './styles';
 import { IconBaseProps } from 'react-icons';
 import { useField } from '@unform/core';
@@ -9,8 +18,26 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest}) => {
-    const inputRef = useRef(null);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const [isFilled, setIsFilled] = useState(false);
+    const [isFocused, setIsFocused] = useState(false);
+
     const { fieldName, defaultValue, error, registerField } = useField(name);
+    
+    const handleInputFocus = useCallback(() => {
+        setIsFocused(true);
+        //pode usar setIsFilled(!!inputRef.current?.value)
+        if (inputRef.current?.value) {
+            setIsFilled(true);
+        }else {
+            setIsFilled(false);
+        }
+    }, []);
+
+    const handleInputBlur = useCallback(() => {
+        setIsFocused(false);
+    }, []);
 
     useEffect(() => {
         registerField({
@@ -21,9 +48,15 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest}) => {
     },[fieldName, registerField]);
 
     return (
-        <Container>
+        <Container isFilled={ isFilled } isFocused={ isFocused }>
             { Icon && <Icon size={ 20 } /> }
-            <input defaultValue={ defaultValue } ref={inputRef} {...rest} />
+            <input 
+                onFocus={ handleInputFocus }
+                onBlur={ handleInputBlur }
+                defaultValue={ defaultValue } 
+                ref={inputRef} 
+                {...rest} 
+            />
         </Container>
     );
 };
--- gobarber-web/src/components/Input/styles.ts
+++ gobarber-web/src/components/Input/styles.ts
@@ -1,22 +1,36 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+    isFocused: Boolean;
+    isFilled: Boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
     font-size: 16px;
     
-    color: #666360;
-    background: #232129;
     
     border-radius: 10px;
-    border: 2px solid #232129;
     padding: 16px;
     width: 100%;
-
     display: flex;
     align-items: center;
+
+    color: #666360;
+    background: #232129;
+    border: 2px solid #232129;
 
     & + div {
             margin-top: 8px;
         }
+
+    ${ props => props.isFocused && css`
+        color: #ff9000;
+        border-color: #ff9000;
+    `}
+
+    ${ props => props.isFilled && css`
+        color: #ff9000;
+    `}
 
     input {
         flex: 1;
