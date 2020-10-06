c06eebf1fa97e08e1d723ae91d786cd3c1fbad20
Claudeilton Dantas <fcd007@hotmail.com>
2020-10-06 08:08:53 -0300

    update verifcation field form

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Container, Content, Background } from './styles';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -9,10 +10,26 @@ import Button from '../../components/Button';
 import logoImg from '../../assets/logo.svg';
 
 const SignUp: React.FC = () => {
+  const  handleSubmit = useCallback(async (data: object) => {
+    try {
+        const schema = Yup.object().shape({
+          name: Yup.string()
+            .required('Nome obrigatório'),
+          email: Yup.string()
+            .required('E-mail obrigatório')
+            .email('Digite um e-mail válido'),
+          password: Yup.string()
+            .min(8, 'Senha com mínimo de 8 caracteres'),
 
-  function handleSubmit(data:object): void {
-    console.log(data);
-  } 
+          });
+
+          await schema.validate(data, {
+            abortEarly: false,
+          });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
       <Container>
