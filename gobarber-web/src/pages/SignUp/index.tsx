8e5bc81144468e61feb1d326308bffe4af4c82d7
Claudeilton Dantas <fcd007@hotmail.com>
2020-10-07 21:37:01 -0300

    add erros validation inputs

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -1,8 +1,11 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Container, Content, Background } from './styles';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+
+import getValidationError from '../../utils/getValidationErros';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -10,6 +13,10 @@ import Button from '../../components/Button';
 import logoImg from '../../assets/logo.svg';
 
 const SignUp: React.FC = () => {
+
+  const formRef = useRef<FormHandles>(null);
+  console.log(formRef);
+
   const  handleSubmit = useCallback(async (data: object) => {
     try {
         const schema = Yup.object().shape({
@@ -26,8 +33,11 @@ const SignUp: React.FC = () => {
           await schema.validate(data, {
             abortEarly: false,
           });
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+
+      const errors= getValidationError(err);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -36,7 +46,8 @@ const SignUp: React.FC = () => {
       <Background />
       <Content>
         <img src={ logoImg } alt="GoBarber Web"/>
-        <Form onSubmit={ handleSubmit }>
+        
+        <Form ref= { formRef } onSubmit={ handleSubmit }>
           <h1>Faça seu cadastro</h1>
           
           <Input name="name" icon={ FiUser } placeholder= "Nome" />
@@ -47,7 +58,7 @@ const SignUp: React.FC = () => {
           
           <Button type="submit">Cadastrar</Button>
 
-        </Form>onSubmit={ }
+        </Form>
 
         <a href="#">
           <FiArrowLeft />
---
+++ gobarber-web/src/utils/getValidationErros.ts
@@ -0,0 +1,16 @@
+import { ValidationError } from 'yup';
+
+interface errorValidationProps {
+    [key: string]: string;
+
+}
+
+export default function getValitdationErros ( err: ValidationError): errorValidationProps {
+    const validationErrors: errorValidationProps = { };
+
+    err.inner.forEach(error => {
+        validationErrors[error.path] = error.message;
+    });
+
+    return validationErrors;
+}
