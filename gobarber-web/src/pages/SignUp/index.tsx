7ab0999e58db527adfb8c39c86c2863929569547
Claudeilton Dantas <fcd007@hotmail.com>
2020-10-08 07:04:43 -0300

    validar input erros correct

--- gobarber-web/src/pages/SignUp/index.tsx
+++ gobarber-web/src/pages/SignUp/index.tsx
@@ -21,6 +21,7 @@ const SignUp: React.FC = () => {
 
   const  handleSubmit = useCallback(async (data: object) => {
     try {
+        formRef.current?.setErrors({});
         const schema = Yup.object().shape({
           name: Yup.string()
             .required('Nome obrigatório'),
