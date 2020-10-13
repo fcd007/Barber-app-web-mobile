2e58e25bf62879a3e62a8808087d4963c18824f0
Claudeilton Dantas <fcd007@hotmail.com>
2020-10-12 22:07:37 -0300

    add AuthContext signIn

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 // eslint-disable-next-line import/extensions
-import SignIn from './pages/SignIn/';
+import SignIn from './pages/SignIn';
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignIn /> 
+    <AuthContext.Provider value={{ name: 'Dantas' }}>
+      <SignIn />
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
--- gobarber-web/src/pages/signIn/index.tsx
+++ gobarber-web/src/pages/signIn/index.tsx
@@ -1,10 +1,11 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
+import AuthContext from '../../context/AuthContext';
 import getValidationError from '../../utils/getValidationErros';
 
 import { Container, Content, Background } from './styles';
@@ -14,56 +15,59 @@ import Button from '../../components/Button';
 import logoImg from '../../assets/logo.svg';
 
 const SignIn: React.FC = () => {
-
   const formRef = useRef<FormHandles>(null);
-  console.log(formRef);
 
-  const  handleSubmit = useCallback(async (data: object) => {
+  const { name } = useContext(AuthContext);
+
+  const handleSubmit = useCallback(async (data: object) => {
     try {
-        formRef.current?.setErrors({});
-        const schema = Yup.object().shape({
-          email: Yup.string()
-            .required('E-mail obrigatório')
-            .email('Digite um e-mail válido'),
-          password: Yup.string()
-            .required('Senha obrigatória'),
-          });
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('E-mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
 
-          await schema.validate(data, {
-            abortEarly: false,
-          });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
     } catch (err) {
-
-      const errors= getValidationError(err);
+      const errors = getValidationError(err);
 
       formRef.current?.setErrors(errors);
     }
   }, []);
 
   return (
-      <Container>
-        <Content>
-          <img src={ logoImg } alt="GoBarber Web"/>
-          <Form ref= { formRef } onSubmit={ handleSubmit }>
-            <h1>Faça seu logon</h1>
-            
-            <Input name="email" icon={ FiMail } placeholder= "E-mail" />
-            
-            <Input name="password" icon={ FiLock } type="password" placeholder="Senha" />
-            
-            <Button type="submit">Entrar</Button>
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber Web" />
+        <Form ref={formRef} onSubmit={handleSubmit}>
+          <h1>Faça seu logon</h1>
 
-            <a href="forgot">Esqueci minha senha</a>
-          </Form>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-          <a href="#">
-            <FiLogIn />
-            Criar conta
-          </a>
-        </Content>
-        <Background />
-      </Container>
-    );
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a href="#">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+      <Background />
+    </Container>
+  );
 };
 
 export default SignIn;
