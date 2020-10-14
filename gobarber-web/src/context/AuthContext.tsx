598deb477fa70d75b81fbaf70774438322cd2a29
Claudeilton Dantas <fcd007@hotmail.com>
2020-10-14 08:30:16 -0300

    add localStorage and Auth

--- gobarber-web/src/context/AuthContext.tsx
+++ gobarber-web/src/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
 import api from '../services/api';
 
 interface SignCredentials {
@@ -10,18 +10,39 @@ interface AuthContextState {
   signIn(credentials: SignCredentials): Promise<void>;
 }
 
+interface AuthState {
+  token: string;
+  user: object;
+}
+
 export const AuthContext = createContext<AuthContextState>(
   {} as AuthContextState,
 );
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@GoBarber:token');
+    const user = localStorage.getItem('@GoBarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
       email,
       password,
     });
 
-    console.log(response.data);
+    const { token, user } = response.data;
+
+    localStorage.setItem('@GoBarber:token', token);
+    localStorage.setItem('@GoBarber:user', JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
