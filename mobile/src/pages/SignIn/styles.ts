a7f10cfa3641b6d15eac4d3f2ffd9ba6d0a6efd3
Claudeilton Dantas <fcd007@hotmail.com>
2020-10-20 18:06:32 -0300

    update fonts assets ands logo app barber

--- mobile/src/pages/SignIn/styles.ts
+++ mobile/src/pages/SignIn/styles.ts
@@ -1,3 +1,15 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+    flex:1;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const Title = styled.Text`
+    font-family: 'RobotoSlab-Medium';
+    color: #f4ede8;
+    font-size: 24px;
+    line-height: 36px;
+    margin: 64px 0 24px;
+`;
