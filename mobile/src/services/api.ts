5ba62ec5ba74655e447a0628ab5534f3a530ebcf
Claudeilton Dantas <fcd007@hotmail.com>
2020-12-19 22:35:29 -0300

    api restore

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -4,7 +4,6 @@ import axios from 'axios';
 // Android Studio = 'http://10.0.2.2:3333'
 // Genymotion     = 'http://10.0.3.2:3333'
 // Android        = 'http://seu-ip:3333'
-
 const api = axios.create({
   baseURL: 'http://10.0.2.2:3333'
 });
