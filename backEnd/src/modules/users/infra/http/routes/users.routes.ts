2c2b9fa181634f662e9c481fd0420e3e93608d28
Claudeilton Dantas <fcd007@hotmail.com>
2020-12-21 19:56:37 -0300

    update domain users and server

--- backEnd/src/modules/users/infra/http/routes/users.routes.ts
+++ backEnd/src/modules/users/infra/http/routes/users.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import multer from 'multer';
-import uploadConfig from '../../../../config/upload';
+import uploadConfig from '@config/upload';
 
-import CreateUserService from '../../../../modules/users/services/CreateUserService';
-import UpdateUserAvatarService from '../../../../modules/users/services/UpdateUserAvatarService';
+import CreateUserService from '@modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
 
-import ensureAutheticaded from '../middlewares/ensureAuthenticated';
+import ensureAutheticaded from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
--- backEnd/src/shared/infra/http/server.ts
+++ backEnd/src/shared/infra/http/server.ts
@@ -3,11 +3,11 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 
-import routes from './routes';
-import uploadConfig from '../../../config/upload';
-import AppError from '../../errors/AppError';
+import routes from '@shared/infra/http/routes';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 
-import '../typeorm';
+import '@shared/infra/typeorm';
 
 const app = express();
 
