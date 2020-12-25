6240b0a0f209fbb027c301017637cedbf2844e50
Claudeilton Dantas <fcd007@hotmail.com>
2020-12-25 07:35:00 -0300

    update date appointments isEqual date-fns

--- backEnd/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backEnd/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,7 @@
 import 'reflect-metadata';
 
+import AppError from '@shared/errors/AppError';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppoitmentService from './CreateAppointmentService';
 
@@ -21,7 +23,22 @@ describe('CreateAppoitment', () => {
     expect(appointment.provider_id).toBe('123456789');
   });
   // test not create new tow appointment same time
-  // it('Shold not be able to create two appoitments on the same time', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('Shold not be able to create two appoitments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppoimentRepository = new CreateAppoitmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const dateAppointment = new Date(2020, 12, 10, 11);
+
+    await createAppoimentRepository.execute({
+      date: dateAppointment,
+      provider_id: '123456789',
+    });
+
+    expect(createAppoimentRepository.execute({
+      date: dateAppointment,
+      provider_id: '123456789',
+    })).rejects.toBeInstanceOf(AppError)
+  });
 });
