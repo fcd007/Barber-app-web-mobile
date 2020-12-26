c44965cc5c5a390330f060f69fda361ed32e2772
Claudeilton Dantas <fcd007@hotmail.com>
2020-12-26 12:29:15 -0300

    update tests jest and providers and avatar

--- backEnd/src/modules/users/services/AutheticateUserService.spec.ts
+++ backEnd/src/modules/users/services/AutheticateUserService.spec.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 
-// import AppError from '@shared/errors/AppError';
+import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '@modules/users/providers/HashProvider/fakes/FakeHashProvider';
@@ -36,5 +36,50 @@ describe('AuthenticateUser', () => {
     });
 
     expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
+  });
+  //authenticate user not existing
+  it('Shold not be able to authenticate with non existing  user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const autheticateUser = new AutheticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+    expect(
+      autheticateUser.execute({
+        email: 'fcd007@hotmail.com',
+        password: '123456789',
+      })).rejects.toBeInstanceOf(AppError)
+  });
+
+  it('Shold not be able to authenticate with wrong password', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider
+    );
+
+    const autheticateUser = new AutheticateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+
+   await createUser.execute({
+      name: 'Claudeilton Dantas',
+      email: 'fcd007@hotmail.com',
+      password: '123456789'
+    });
+
+    expect(
+      autheticateUser.execute({
+        email: 'fcd007@hotmail.com',
+        password: 'password-error:6789',
+      })
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
--- backEnd/src/modules/users/services/UpdateUserAvatarService.ts
+++ backEnd/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,10 +1,8 @@
-import path from 'path';
-import fs from 'fs';
 import { injectable, inject } from 'tsyringe';
 
 //SOLID - using dependency inversion
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import uploadConfig from '@config/upload';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import AppError from '@shared/errors/AppError';
 import User from '../infra/typeorm/entities/User';
 
@@ -19,6 +17,9 @@ class UpdateUserAvatarService {
   constructor (
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider,
   ) { }
 
   public async execute({ user_id, avatarFileName }: IRequest): Promise<User> {
@@ -31,15 +32,12 @@ class UpdateUserAvatarService {
 
     if(user.avatar) {
       //deletar avatar anterior
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if(userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
-      }
+      this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFileName;
+    const fileName = await this.storageProvider.saveFile(avatarFileName);
+
+    user.avatar = fileName;
 
     await this.usersRepository.save(user);
 
