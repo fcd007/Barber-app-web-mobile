1598c333ee115a47e2c62d9e293284181f0cf14e
Claudeilton Dantas <fcd007@hotmail.com>
2020-12-28 16:30:47 -0300

    add send mail fake library nodemailer

--- backEnd/src/shared/container/providers/index.ts
+++ backEnd/src/shared/container/providers/index.ts
@@ -3,7 +3,16 @@ import { container } from 'tsyringe';
 import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
 
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import EtherealEmailProvider from '@shared/container/providers/MailProvider/implementations/EtherealEmailProvider';
+
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
+);
+
+container.registerSingleton<IMailProvider>(
+  'MailProvider',
+  EtherealEmailProvider,
 );
