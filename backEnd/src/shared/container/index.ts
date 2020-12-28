6a773f222816898338e173d47f8e6bc5d202dcb8
Claudeilton Dantas <fcd007@hotmail.com>
2020-12-28 15:28:54 -0300

    register depository userTokens app

--- backEnd/src/shared/container/index.ts
+++ backEnd/src/shared/container/index.ts
@@ -3,14 +3,14 @@ import { container } from  'tsyringe';
 import '@modules/users/providers';
 import '@shared/container/providers/';
 
-import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository'
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository'
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
-import IUsersRepository from '@modules/users/repositories/IUsersRepository'
-import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository'
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
-// import IUsersTokensRepository from '@modules/users/repositories/IUsersTokensRepository'
-// import UsersTokensRepository from '@modules/users/infra/typeorm/repositories/UsersTokensRepository'
+import IUsersTokensRepository from '@modules/users/repositories/IUsersTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository'
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
@@ -22,7 +22,7 @@ container.registerSingleton<IUsersRepository>(
   UsersRepository
  );
 
-//  container.registerSingleton<IUsersTokensRepository>(
-//   'UsersTokensRepository',
-//   UsersTokensRepository
-//  );
+ container.registerSingleton<IUsersTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository
+ );
