41da66d9f9007b132c33997b30d3306ce74c6433
Claudeilton Dantas <fcd007@hotmail.com>
2021-01-02 16:16:50 -0300

    add route profile errors

--- backEnd/src/modules/users/infra/http/controllers/ProfileController.ts
+++ backEnd/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -6,30 +6,38 @@ import ShowProfileService from '@modules/users/services/ShowProfileService'
 
 export default class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
-    const user_id = await request.user.id;
+    const user_id = request.user.id;
 
     const showProfile = container.resolve(ShowProfileService);
 
     const user = await showProfile.execute({ user_id });
 
+    // delete user.password;
+
     return response.json(user);
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
-      const user_id = await request.user.id;
-      const { name , email, old_password, password } = request.body;
+      const user_id = request.user.id;
+      console.log(user_id);
+
+      const {
+        name ,
+        email,
+        old_password,
+        password
+      } = request.body;
 
       const updateProfile = container.resolve(UpdateProfileService);
 
-     const user = await updateProfile.execute({
-       user_id,
-        email,
-        name,
-        old_password,
-        password,
-     });
-
-     delete user.password;
+      const user = await updateProfile.execute({
+          user_id,
+          email,
+          name,
+          old_password,
+          password,
+      });
+      console.log(user);
 
      return response.json(user);
     }
--- backEnd/src/modules/users/infra/http/routes/profile.routes.ts
+++ backEnd/src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 
 import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
+
 import ensureAutheticaded from '../middlewares/ensureAuthenticated';
 
 const profileRouter = Router();
 const profileController = new ProfileController();
 
 profileRouter.use(ensureAutheticaded);
+
+profileRouter.get('/', profileController.show);
 profileRouter.put('/', profileController.update);
 
 export default profileRouter;
