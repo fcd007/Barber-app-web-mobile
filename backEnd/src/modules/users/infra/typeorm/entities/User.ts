9b6a158d78917d451035b90e06d26f230b254bd4
Claudeilton <fcd007@hotmail.com>
2021-01-08 07:11:56 -0300

    update add class-transformer to class atributes

--- backEnd/src/modules/users/infra/typeorm/entities/User.ts
+++ backEnd/src/modules/users/infra/typeorm/entities/User.ts
@@ -6,6 +6,8 @@ import {
   UpdateDateColumn
 } from 'typeorm';
 
+import { Exclude, Expose } from 'class-transformer';
+
 // KISS - Keep It Simple & Stupid
 
 @Entity('users')
@@ -20,10 +22,16 @@ class User {
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 
   @Column()
   avatar: string;
+
+  @Expose({ name: 'avatar_url'})
+  getAvatar_url(): string | null {
+    return this.avatar ? `${process.env.APP_API_URL}/files/${this.avatar}` : null;
+  }
 
   @CreateDateColumn()
   created_at: Date;
