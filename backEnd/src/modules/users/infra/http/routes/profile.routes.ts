c465e819e66eefec639dce832d8239622922f18d
Claudeilton <fcd007@hotmail.com>
2021-01-07 21:14:42 -0300

    sessions and users validate fields

--- backEnd/src/modules/users/infra/http/routes/profile.routes.ts
+++ backEnd/src/modules/users/infra/http/routes/profile.routes.ts
@@ -17,7 +17,7 @@ profileRouter.get('/',
       email: Joi.string().email().required(),
       old_password: Joi.string(),
       password: Joi.string(),
-      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
     },
   }),
 profileController.show);
