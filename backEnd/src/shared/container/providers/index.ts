f657eb28329103d2fb1107ba4f2db9c86538b283
Claudeilton <fcd007@hotmail.com>
2021-01-10 22:02:05 -0300

    refactore code clean containers providers

--- backEnd/src/shared/container/providers/index.ts
+++ backEnd/src/shared/container/providers/index.ts
@@ -1,27 +1,4 @@
-import { container } from 'tsyringe';
-import mailConfig from '@config/mail';
-
-import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
-import DiskStorageProvider from '@shared/container/providers/StorageProvider/implementations/DiskStorageProvider';
-
-import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
-import EtherealEmailProvider from '@shared/container/providers/MailProvider/implementations/EtherealEmailProvider';
-import SESMailProvider from '@shared/container/providers/MailProvider/implementations/SESMailProvider';
-
-import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
-import HandlebarsMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
-
-container.registerSingleton<IStorageProvider>(
-  'StorageProvider',
-  DiskStorageProvider,
-);
-
-container.registerSingleton<IMailTemplateProvider>(
-  'MailTemplateProvider',
-  HandlebarsMailTemplateProvider,
-  );
-
-  container.registerInstance<IMailProvider>(
-    'MailProvider',
-     mailConfig.driver === 'ethereal' ? container.resolve(EtherealEmailProvider) : container.resolve(SESMailProvider)
-  );
+//Imports inject dependencies providers
+import './StorageProvider/';
+import './MailTemplateProvider/';
+import './MailProvider/';
